185e6a43e59cee5062a9b20c8f97cb6db2b61864
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2020-05-29 21:08:22 -0400

    Commit único

--- BaseDeDatosCrear/Controllers/HomeController.cs
+++ BaseDeDatosCrear/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BaseDeDatosCrear.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace BaseDeDatosCrear.Controllers
 {
     public class HomeController : Controller
     {
+        private EjemploASPNETEntities db = new EjemploASPNETEntities();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +27,22 @@ namespace BaseDeDatosCrear.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult EjemploCreacion() {//La vista general donde se muestra la lista de productos
+            ViewBag.productos = db.Producto.ToList();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Agregar()//La vista para agregar un nuevo registro (formulario)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Agregar(Producto prod) {
+            db.Producto.Add(prod);//Acá agrega el nuevo objeto al conjunto de datos recibidos de la DDBB
+            db.SaveChanges();//Acá actualiza la base de datos.
+            return RedirectToAction("EjemploCreacion");
         }
     }
 }
